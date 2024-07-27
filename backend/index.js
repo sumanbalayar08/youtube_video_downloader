df85941c232c5ee801d1f79e81f0474fcf82f938
sumanbalayar08 <orthohydrogen123@gmail.com>
2024-07-27 15:22:18 +0545

    fix backned

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,57 @@
 const express = require("express")
-
+const fs = require('fs')
+const ytdl = require('ytdl-core')
 const app=express()
+const cors = require("cors")
+const os = require("os")
+const path = require('path');
+
+
+app.use(express.json());
+app.use(cors());
+
+const getDownloadsPath = () => {
+    const homeDir = os.homedir();
+    switch (os.platform()) {
+      case 'win32':
+        return path.join(homeDir, 'Downloads');
+      case 'darwin':
+        return path.join(homeDir, 'Downloads');
+      case 'linux':
+        return path.join(homeDir, 'Downloads');
+      default:
+        throw new Error('Unsupported OS');
+    }
+  };
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 })
+
+app.post('/api/download', async(req, res) => {
+    const { url } = req.body;
+    console.log('Received URL:', url);
+    if(ytdl.validateURL(url)){
+        const videoId= ytdl.getURLVideoID(url);
+        const downloadsPath = getDownloadsPath();
+        const outputPath = path.join(downloadsPath, `video_${videoId}.mp4`);
+        try{
+            await ytdl(url,{quality:'highest'}).pipe(fs.createWriteStream(outputPath))
+            .on('finish', () => {
+                console.log('Download completed');
+                res.status(200).send({ message: 'Download completed successfully', path: outputPath });
+              }).on('error',(err)=>{
+                console.error('Error downloading the video:', err);
+                res.status(500).send({ message: 'Error downloading the video', error: err });
+              })
+        }catch(err){
+            console.error('Error processing the download:', err);
+            res.status(500).send({ message: 'Error processing the download', error: err });
+        }
+    }else {
+        res.status(400).send({ message: 'Invalid URL' });
+      }
+  });
 
 app.listen(4000,()=>{
 try{
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,58 @@
+"use client";
+
 import Image from "next/image";
+import { useState, ChangeEvent } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 export default function Home() {
+  const [url, setUrl] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  const Download = async () => {
+    if (url) {
+      try {
+        const res = await axios.post('http://localhost:4000/api/download', { url });
+        toast.success('Video download initiated successfully!');
+      } catch (err) {
+        console.error(`Error Downloading the Video ${err}`);
+      }
+    } else {
+      //console.log(url);
+      toast.error("Please Enter the URL!");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen space-y-8 bg-gradient-to-tr bg-cyan-100">
       <div className="flex items-center justify-center">
         <Image src="/images/youtube.png" width={100} height={100} alt="car" />
       </div>
       <div className="flex flex-col items-center justify-center space-y-4">
-        <p className="text-center text-7xl font-bold">Youtube Video Downloader</p>
+        <p className="text-center text-7xl font-bold">
+          Youtube Video Downloader
+        </p>
         <p className="text-center text-xl text-zinc-500 font-extralight">
           Paste the YouTube video URL and instantly download videos for offline
           viewing.
         </p>
       </div>
       <div className="flex space-x-4">
-        <input placeholder="Paste your Youtube Video URL" className="p-4 w-96 rounded-lg border-solid border-gray-400 border-2" />
-        <button className="flex p-4 rounded-md bg-green-600 text-white font-semibold">Download</button>
+        <input
+          placeholder="Paste your Youtube Video URL"
+          value={url}
+          onChange={handleChange}
+          className="p-4 w-96 rounded-lg border-solid border-gray-400 border-2"
+        />
+        <button
+          className="flex p-4 rounded-md bg-green-600 text-white font-semibold"
+          onClick={Download}
+        >
+          Download
+        </button>
       </div>
     </div>
   );
